e46422c1c1b5bed2e7725bcbbffe451a8aa7dfe1
Чуева Елизавета <chueva@sfedu.ru>
2021-10-21 17:23:08 +0300

    Added function 'LaterInYear'.

--- DateTime.pas
+++ DateTime.pas
@@ -2,7 +2,7 @@
  
  function SecondsInMinute() : integer := 60;
 
-function SecondInMinute(): integer := 60;     //В минуте всегда  60 секунд.
+
 
 function LaterInYear(DAY1, MONTH1, DAY2, MONTH2: integer): integer;
 begin
