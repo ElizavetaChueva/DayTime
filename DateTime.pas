0f24d344d0d19420464be511f043fbb3de121d0f
Чуева Елизавета <chueva@sfedu.ru>
2021-10-21 20:01:43 +0300

    Added function 'SecondsInHours'

--- DateTime.pas
+++ DateTime.pas
@@ -28,7 +28,7 @@ end;
 
 function SecondsInHours(hours : integer): integer := hours * 60 * 60;
 
-function IsYearOfApocalypse(year : integer) : boolean := (year = 1992) or (year = 2005) or (year = 2011)
+
 
 begin
 
